c28160e37e1469daa5fba5e9363917ad4f0b596c
mahi-14 <93117594+mahi-14@users.noreply.github.com>
2023-04-17 09:35:22 +0530

    ecommerce

---
+++ server/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/RegisterLogin")
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch(() => {
+    console.log("failed");
+  });
+
+const newSchema = new mongoose.Schema({
+  firstname: {
+    type: "String",
+    required: true,
+  },
+  lastname: {
+    type: "String",
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const collection = mongoose.model("collection", newSchema);
+
+module.exports = collection;
